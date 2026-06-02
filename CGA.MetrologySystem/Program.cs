c6fe513669c12a3ef66db6fb09f05be821572e78
JossuEL55 <jossueayala555@gmail.com>
2026-06-02 16:07:01 -0500

    Docker y Kubernetes

--- CGA.MetrologySystem/Program.cs
+++ CGA.MetrologySystem/Program.cs
@@ -9,6 +9,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        dbContext.Database.Migrate();
+    }
+}
 
 if (!app.Environment.IsDevelopment())
 {
